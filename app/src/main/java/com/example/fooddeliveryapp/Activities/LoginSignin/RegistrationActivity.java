11690bb367cce5754c6a649e5ff5b41a8d301e8b
PhamDat-2003 <dattien1512003@gmail.com>
2023-08-05 09:55:51 +0700

    Merge branch 'HoangBranch' into LoginSignIn

--- app/src/main/java/com/example/fooddeliveryapp/Activities/LoginSignin/RegistrationActivity.java
+++ app/src/main/java/com/example/fooddeliveryapp/Activities/LoginSignin/RegistrationActivity.java
@@ -1,17 +1,77 @@
 package com.example.fooddeliveryapp.Activities.LoginSignin;
 
 import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.fooddeliveryapp.Activities.HomePage.HomePage;
 import com.example.fooddeliveryapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
-
+    EditText edtEmail,edtPassword;
+    Button btnReg;
+    FirebaseAuth mAuthReg;
+    TextView txvLog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        mAuthReg=FirebaseAuth.getInstance();
+        edtEmail=findViewById(R.id.edtEmailReg);
+        edtPassword=findViewById(R.id.edtPasswordReg);
+        btnReg=findViewById(R.id.btnReg);
+        txvLog=findViewById(R.id.txvLog);
+
+        txvLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
+                email= String.valueOf(edtEmail.getText().toString());
+                password= String.valueOf(edtPassword.getText().toString());
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(RegistrationActivity.this,"Enter Email",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(RegistrationActivity.this,"Enter Password",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuthReg.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(RegistrationActivity.this, "Account created.",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(RegistrationActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
